bf0eb7763b153ee42edb45ed1b45787e62fc918c
Br01t <giacomo.piccinini1@gmail.com>
2026-03-11 10:54:54 +0100

    fix: remove BrowserRouter basename for Netlify root deployment

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/san-raffaele-stays/">
+      <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Index />} />
